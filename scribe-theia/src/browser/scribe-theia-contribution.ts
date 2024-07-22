d5f608e701bde8d30e13ea32d52658e2e8a18627
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-07-22 12:02:33 +0530

    working electron dev

--- scribe-theia/src/browser/scribe-theia-contribution.ts
+++
@@ -1,7 +0,0 @@
-import { injectable } from '@theia/core/shared/inversify';
-
-@injectable()
-// Add contribution interface to be implemented, e.g. "ScribeTheiaContribution implements CommandContribution"
-export class ScribeTheiaContribution{
-
-}
